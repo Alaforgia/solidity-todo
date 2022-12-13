afc48f81d77505b7fbf6c734d928e7a70e0cbfe8
Tony LaForgia <alforgia428@gmail.com>
2022-12-13 14:53:18 -0600

    created TodoList contract

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -1,3 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+contract ToDoList{
+  uint256 public _idUser;
+  address public ownerOfContract;
+
+  address[] public creators;
+  string[] public message;
+  uint256[] public messageId;
+
+
+struct ToDoListApp{
+  
+}
+
+
+}
