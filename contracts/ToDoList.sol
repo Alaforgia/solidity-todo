47cb077d2627c4eda18fc5644d1f770c7b0f37cf
Tony LaForgia <alforgia428@gmail.com>
2022-12-13 15:12:55 -0600

    created ToDoEvent event

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -9,13 +9,25 @@ contract ToDoList{
   string[] public message;
   uint256[] public messageId;
 
-
+// A struct is a user-defined complex data type that allows you to define a type that has multiple fields, each with its own type.
+// Structs are similar to objects in other programming languages, and they are commonly used to group related data together and 
+// give it a meaningful name.
 struct ToDoListApp{
   address account;
   uint256 userId;
   string message;
   bool completed;
 }
+
+// An event is a special kind of function that allows you to log information about what is happening in your smart contract. 
+// When an event is triggered, the information that it logs is stored in the blockchain's transaction log and can be accessed by 
+// external applications.
+event ToDoEvent (
+  address indexed account,
+  uint256 indexed userId,
+  string message,
+  bool completed
+);
 
 
 }
